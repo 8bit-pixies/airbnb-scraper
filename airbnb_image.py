c31f609387bb484b9fdae939a5f0f1456fc6b9de
Chapman Siu <chapm0n.siu@gmail.com>
2018-04-10 23:06:39 +1000

    fix up

--- airbnb_image.py
+++ airbnb_image.py
@@ -4,38 +4,10 @@ import urllib.request
 import os
 from bs4 import BeautifulSoup
 
-sample_page = 'https://www.airbnb.com.au/rooms/18285386'
-page = requests.get(sample_page)
-
-soup = BeautifulSoup(page.content, 'html.parser')
-
-# download all images that are under class slideshow images pattern
-all_img = soup.find_all("img")
-
-for img in all_img:
-    #print(img['src'])
-    try:
-        img_nm = img['src']
-        if 'jpg' in img_nm:
-            #print(img_nm)
-            img_loc, type_pic = img_nm.split(".jpg", 1)
-            if 'profile' in type_pic:
-                continue
-            basenm = os.path.basename(img_loc)+".jpg"
-            img_loc = img_loc+".jpg"
-            urllib.request.urlretrieve(img_loc, basenm)
-            #print("tried to download: ", img_loc)
-        else:
-            pass
-            #print(img_nm)
-    except Exception as e:
-        print(e)
-
 def get_images(airbnb_id):
     page_path = "https://www.airbnb.com.au/rooms/{}".format(airbnb_id)
     page = requests.get(page_path)
     soup = BeautifulSoup(page.content, 'html.parser')
-
     all_img = soup.find_all("img")
     
     for idx, img in enumerate(all_img):
@@ -59,4 +31,5 @@ def get_images(airbnb_id):
         except Exception as e:
             print(e)
 
-get_images('18285386')
+if __name__ == '__main__':
+    get_images('18285386')
